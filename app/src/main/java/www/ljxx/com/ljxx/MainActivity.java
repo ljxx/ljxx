0a803be7974458db09bb56e2e0e4c5fec8dbdd47
yanglixiang <QNjingling1108@163.com>
2016-03-11 16:04:48 +0800

    增加dev分支并提交内容

--- app/src/main/java/www/ljxx/com/ljxx/MainActivity.java
+++ app/src/main/java/www/ljxx/com/ljxx/MainActivity.java
@@ -54,4 +54,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onPause() {
         super.onPause();
     }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
 }
